a152b190e3f5da3593cc36c67f03c384712a4023
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-02 17:41:38 +0300

    v5

---
+++ mpris_feature.py
@@ -0,0 +1,155 @@
+# mpris_feature.py
+
+import re
+import urllib.parse
+
+try:
+    import dbus
+    import dbus.service
+    import dbus.mainloop.glib
+    from gi.repository import GLib
+except ImportError:
+    pass
+
+class HardPlayerMPRIS(dbus.service.Object):
+    def __init__(self, main_win):
+        # إعداد الحلقة الرئيسية لـ D-Bus
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        bus_name = dbus.service.BusName('org.mpris.MediaPlayer2.hardplayer', bus=dbus.SessionBus())
+        super(HardPlayerMPRIS, self).__init__(bus_name, '/org/mpris/MediaPlayer2')
+        
+        self.main_win = main_win
+        self.player = main_win.player
+
+        # مراقبة خصائص MPV لإرسال تحديثات فورية للنظام
+        self.player.observe_property('pause', self.on_pause_change)
+        self.player.observe_property('media-title', self.on_metadata_change)
+        self.player.observe_property('duration', self.on_metadata_change) # مطلوب لظهور شريط التقدم
+
+    # --- إشارات D-Bus (Signals) ---
+    @dbus.service.signal('org.freedesktop.DBus.Properties', signature='sa{sv}as')
+    def PropertiesChanged(self, interface, changed_properties, invalidated_properties):
+        pass
+
+    @dbus.service.signal('org.mpris.MediaPlayer2.Player', signature='x')
+    def Seeked(self, position):
+        pass
+
+    # --- دوال المراقبة (Observers) ---
+    def on_pause_change(self, name, value):
+        status = 'Paused' if value else 'Playing'
+        GLib.idle_add(self.PropertiesChanged, 'org.mpris.MediaPlayer2.Player', {'PlaybackStatus': status}, [])
+
+    def on_metadata_change(self, name, value):
+        metadata = self.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+        GLib.idle_add(self.PropertiesChanged, 'org.mpris.MediaPlayer2.Player', {'Metadata': metadata}, [])
+
+    # --- دوال التحكم الأساسية (Methods) ---
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='', out_signature='')
+    def PlayPause(self): 
+        self.player.pause = not self.player.pause
+    
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='', out_signature='')
+    def Play(self): 
+        self.player.pause = False
+    
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='', out_signature='')
+    def Pause(self): 
+        self.player.pause = True
+    
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='', out_signature='')
+    def Stop(self): 
+        self.player.stop()
+
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='', out_signature='')
+    def Next(self): 
+        GLib.idle_add(self.main_win.play_next)
+
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='', out_signature='')
+    def Previous(self): 
+        GLib.idle_add(self.main_win.play_previous)
+
+    # --- دوال شريط التقدم (التقديم والتأخير) ---
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='x', out_signature='')
+    def Seek(self, offset):
+        # offset يأتي بالمايكروثانية
+        current = getattr(self.player, 'time_pos', 0)
+        if current is not None:
+            new_pos = current + (offset / 1000000.0)
+            self.player.time_pos = max(0, new_pos)
+            GLib.idle_add(self.Seeked, int(self.player.time_pos * 1000000))
+
+    @dbus.service.method('org.mpris.MediaPlayer2.Player', in_signature='ox', out_signature='')
+    def SetPosition(self, track_id, position):
+        # position يأتي بالمايكروثانية
+        self.player.time_pos = position / 1000000.0
+        GLib.idle_add(self.Seeked, position)
+
+    # --- إرسال الخصائص للنظام (Properties) ---
+    @dbus.service.method('org.freedesktop.DBus.Properties', in_signature='ss', out_signature='v')
+    def Get(self, interface, prop):
+        if interface == 'org.mpris.MediaPlayer2':
+            if prop == 'Identity': return 'HardPlayer'
+            if prop == 'CanQuit': return True
+        
+        if interface == 'org.mpris.MediaPlayer2.Player':
+            if prop == 'PlaybackStatus': 
+                return 'Paused' if self.player.pause else 'Playing'
+            
+            # جلب الوقت الحالي للفيديو
+            if prop == 'Position': 
+                pos = getattr(self.player, 'time_pos', 0)
+                return dbus.Int64((pos or 0) * 1000000)
+
+            # البيانات الوصفية (العنوان، الصورة المصغرة، المدة)
+            if prop == 'Metadata':
+                title = getattr(self.player, 'media_title', "HardPlayer") or "HardPlayer"
+                path = getattr(self.player, 'path', "") or ""
+                
+                meta = {
+                    'mpris:trackid': dbus.ObjectPath('/org/mpris/MediaPlayer2/TrackList/NoTrack'),
+                    'xesam:title': dbus.String(title)
+                }
+
+                # جلب مدة الفيديو الإجمالية
+                duration = getattr(self.player, 'duration', 0)
+                if duration:
+                    meta['mpris:length'] = dbus.Int64(duration * 1000000)
+
+                # جلب الصورة المصغرة ليوتيوب أو مسار الملف المحلي
+                if path.startswith('http') and ('youtube.com' in path or 'youtu.be' in path):
+                    match = re.search(r'(?:v=|youtu\.be\/)([^&]+)', path)
+                    if match:
+                        vid_id = match.group(1)
+                        thumb_url = f"https://img.youtube.com/vi/{vid_id}/hqdefault.jpg"
+                        meta['mpris:artUrl'] = dbus.String(thumb_url)
+                elif path and not path.startswith('http'):
+                    meta['xesam:url'] = dbus.String(f"file://{urllib.parse.quote(path)}")
+
+                return dbus.Dictionary(meta, signature='sv')
+            
+            # السماحيات لتفعيل الأزرار في الواجهات
+            if prop == 'CanGoNext': return True
+            if prop == 'CanGoPrevious': return True
+            if prop == 'CanControl': return True
+            if prop == 'CanPause': return True
+            if prop == 'CanPlay': return True
+            if prop == 'CanSeek': return True
+            
+        return ""
+
+    @dbus.service.method('org.freedesktop.DBus.Properties', in_signature='s', out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface == 'org.mpris.MediaPlayer2.Player':
+            return {
+                'PlaybackStatus': self.Get(interface, 'PlaybackStatus'),
+                'Metadata': self.Get(interface, 'Metadata'),
+                'Position': self.Get(interface, 'Position'),
+                'CanGoNext': True,
+                'CanGoPrevious': True,
+                'CanControl': True,
+                'CanPause': True,
+                'CanPlay': True,
+                'CanSeek': True
+            }
+        return {}
