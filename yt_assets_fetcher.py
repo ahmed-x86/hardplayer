1469ce8ab1501e4e1224d28197b0ff1bb627138e
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-11 15:18:51 +0300

    v28.0.0: Update yt_assets_fetcher.py

---
+++ yt_assets_fetcher.py
@@ -0,0 +1,20 @@
+# yt_assets_fetcher.py
+
+import urllib.request
+from PyQt6.QtCore import QThread, pyqtSignal
+
+class ImageFetcher(QThread):
+    image_fetched = pyqtSignal(str, bytes)
+
+    def __init__(self, url, identifier):
+        super().__init__()
+        self.url = url
+        self.identifier = identifier
+
+    def run(self):
+        try:
+            req = urllib.request.Request(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+            data = urllib.request.urlopen(req).read()
+            self.image_fetched.emit(self.identifier, data)
+        except Exception:
+            pass # نتجاهل الخطأ إذا لم تتوفر الصورة
