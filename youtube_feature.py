7b1dd2607f791a1bc3f5dc19ee8eb35729169664
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-16 10:15:51 +0300

    v28.0.0: Update youtube_feature.py

--- youtube_feature.py
+++ youtube_feature.py
@@ -1,22 +1,22 @@
 # youtube_feature.py
 
 """
-واجهة التجميع (Facade Pattern):
-هذا الملف يقوم بتجميع الدوال والأصناف من الملفات المقسمة (البنية التحتية والنوافذ)
-حتى لا تضطر لتغيير أي شيء في ملفات المشروع الأخرى مثل (main_window.py وغيرها).
+Facade Pattern:
+This file aggregates functions and classes from the split files (infrastructure and dialogs)
+so that you do not have to change anything in other project files like (main_window.py and others).
 """
 
-# استدعاء ملفات الـ Backend
+# Import Backend files
 from yt_url_parser import clean_ansi, clean_youtube_url
 from yt_assets_fetcher import ImageFetcher
 from yt_info_fetcher import YTInfoFetcher
 from yt_downloader import DownloadWorker
 from yt_search_engine import YTSearchEngine
 
-# استدعاء ملف النوافذ الجديد (Dialogs)
+# Import the new windows file (Dialogs)
 from yt_dialogs import YouTubeSimpleQualityDialog, YouTubeQualityDialog, YouTubeSearchDialog
 
-# تحديد ما سيتم تصديره لباقي المشروع بشكل مباشر
+# Specify what will be exported directly to the rest of the project
 __all__ = [
     "clean_ansi",
     "clean_youtube_url",
